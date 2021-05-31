b1d46d5089a698aca45f987fd6a5fd480932fda9
toystad <85112755+toystad@users.noreply.github.com>
2021-05-31 14:15:07 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+
+while(krone === 0) {
+    document.writeln("Kron! Vi flipper igjen...");
+    var krone = Math.floor(Math.random()*2);
+}
+document.writeln("Mynt! Gratulerer!");
